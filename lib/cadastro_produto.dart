165f37b125b1320bd40f4d25bcb339e48868162e
Lucas de Almeida <lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.comgit config --global user.email lucasde.almeida@hotmail.com>
2020-07-22 08:37:20 -0300

    adicionado opção camera

--- lib/cadastro_produto.dart
+++ lib/cadastro_produto.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:cadastro_produtos/data/mock_data.dart';
 import 'package:cadastro_produtos/produto.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CasdastroProduto extends StatefulWidget {
   static String routeName = '/cadastro';
@@ -12,6 +15,19 @@ class _CasdastroProdutoState extends State<CasdastroProduto> {
   TextEditingController _nome = TextEditingController();
   TextEditingController _descricao = TextEditingController();
   Produto produto = Produto();
+  final _imagem = ImagePicker();
+  File _arquivo;
+
+  Future<void> _tiraFoto(ImageSource source) async {
+    var _pickFile = await _imagem.getImage(
+      source: source,
+    );
+    if (_pickFile != null) {
+      setState(() {
+        _arquivo = File(_pickFile.path);
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -32,9 +48,51 @@ class _CasdastroProdutoState extends State<CasdastroProduto> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro de produtos'),
+        centerTitle: true,
       ),
       body: ListView(
         children: <Widget>[
+          //colocar o campo de imagem e botao de tirar e selecionar foto Image.file(File(path)) ,FileImage(File(path))
+          Column(
+            children: <Widget>[
+              _arquivo == null
+                  ? Text('Nenhuma foto encontrada')
+                  : FadeInImage(
+                      placeholder: AssetImage('assets/loading.gif'),
+                      image: FileImage(_arquivo),
+                    ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RaisedButton.icon(
+                      onPressed: () => _tiraFoto(
+                        ImageSource.camera,
+                      ),
+                      icon: Icon(Icons.camera_alt),
+                      label: Text('Tirar foto'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: RaisedButton.icon(
+                      onPressed: () => _tiraFoto(
+                        ImageSource.gallery,
+                      ),
+                      icon: Icon(
+                        Icons.photo_library,
+                      ),
+                      label: Text('Selecionar foto'),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
           Form(
               child: Column(
             children: <Widget>[
